c7bdd89fe35ebe497c20e695eb5596136de02ed9
Zachary <zachary.zxj@outlook.com>
2016-05-30 17:08:34 +0800

    canshu

---
+++ shell/2canshu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+printf "the complete list is %s\n" "$$"
+printf "the complete list is %s\n" "$!"
+printf "the complete list is %s\n" "$?"
+printf "the complete list is %s\n" "$*" 
+printf "the complete list is %s\n" "$@" 
+printf "the complete list is %s\n" "$#" 
+printf "the complete list is %s\n" "$0" 
+printf "the complete list is %s\n" "$1"
+printf "the complete list is %s\n" "$2"  
