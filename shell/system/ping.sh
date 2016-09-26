25f1a2803f33e41d9f6f319bcab7a26f7e789fd3
秦明 <zard@local.com>
2016-09-26 16:42:42 +0800

    ping

--- shell/system/ping.sh
+++ shell/system/ping.sh
@@ -7,13 +7,13 @@ NETWORK=$1
 YESTERDAY=$(date -d yesterday +%Y-%m-%d)
 for HOST in $(seq 1 16)
 do
-    /usr/bin/ping -c1 -w1 $NETWORK.$HOST &>/dev/null && RESULT=0 || RESULT=1
+    /usr/bin/ping -c1 -w1 $NETWORK.$HOST &> /dev/null && RESULT=0 || RESULT=1
         if [ "$RESULT" == 0 ];then
             echo -e "\033[32;1m$NETWORK.$HOST is up \033[0m"
-            echo "$NETWORK.$HOST" >> /shell/$YESTERDAY-UP.txt
+            echo "$NETWORK.$HOST" >> /opt/$YESTERDAY-UP.txt
         else
             echo -e "\033[;31m$NETWORK.$HOST is down \033[0m"
-            echo "$NETWORK.$HOST" >> /shell/$YESTERDAY-DOWN.txt
+            echo "$NETWORK.$HOST" >> /opt/$YESTERDAY-DOWN.txt
         fi
 done
 
